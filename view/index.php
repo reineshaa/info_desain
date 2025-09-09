50535d46405e1c50a4f0dd385112e477f92a3d2e
reineshaa <ratudasha.7@gmail.com>
2025-09-09 08:06:14 +0700

    updated code

---
+++ view/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include __DIR__ . "/view/navbar.utils.php"; ?>
+
+    <?php
+    include __DIR__ . "/../controller/controller.php";
+    include __DIR__ . "/view/view.php";
+
+    $projects = new View();
+    ?>
+
+    <?php $projects->find(); ?>
+
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>JUDUL</th>
+            <th>TEMA</th>
+            <th>LINK</th>
+            <th>DESAINER</th>
+            <th>CLIENT</th>
+        </tr>
+        <?php $projects->show(); ?>
+    </table>
+</body>
+</html>

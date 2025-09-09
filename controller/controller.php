90a66c4855d0df5db9aa2e8123a2650297e0ff27
reineshaa <ratudasha.7@gmail.com>
2025-09-09 07:41:46 +0700

    updated code

---
+++ controller/controller.php
@@ -0,0 +1,19 @@
+<?php
+
+class Controller extends Model{
+    public function getProjects(){
+        return $this->findAll();
+    }
+    
+    public function getProjectsBy(){
+        if(!isset($_GET['id'])){
+            return;
+        }
+
+        $onlyId = preg_replace('/\D/', '', $_GET['id']);
+        if($onlyId !== ""){
+            return $this->findOne($_GET['id']);
+        }
+    }
+}
+?>
